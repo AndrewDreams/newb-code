e7758f1662fd6e53e09ed0d29a6766d11e1473a6
andrewdreams <andrewdof@gmail.com>
2022-02-20 08:16:44 -0600

    fixed comma issue

--- acipco-rates.js
+++ acipco-rates.js
@@ -9,66 +9,64 @@ const acipcoRates = {
     "colorado": 3.30,
     "connecticut": 3.85,
     "delaware": 3.85,
-    // dc: 3.85
-    // floridaNorth: 3.40
-    // floridaSouth: 3.80
-    // georgia: 3.25
-    // idaho: 2.80
-    // illinois: 3.25
-    // indiana: 3.40
-    // iowa: 2.80
-    // kansas: 2.90
-    // kentuckyEast: 4.05
-    // kentuckyWest: 3.80
-    // louisiana: 3.30
-    // maine: 3.85
-    // maryland: 3.85
-    // massachusettes: 3.85
-    // michiganNorth: 3.70
-    // michiganSouth: 3.20
-    // minnesota: 3.25
-    // mississippi: 3
-    // missouriEast: 3.50
-    // missouriWest: 3.60
-    // montana: 3.25
-    // nebraska: 2.90
-    // nevada: 2.75
-    // newHampshire: 3.85
-    // newJersey: 3.85
-    // newMexico: 2.95
-    // newYorkEast: 4
-    // newYorkWest: 3.85
-    // northDakota: 3.65
-    // ohio: 3.25
-    // oklahoma: 3.25
-    // oregon: 2.75
-    // pennsylvaniaEast: 3.70
-    // pennsylvaniaWest: 3.10
-    // rhodeIsland: 3.85
-    // southCarolina: 3.50
-    // southDakota: 3.20
-    // tennessee: 3
-    // texasEast: 3.10
-    // texasWest: 3.50
-    // utah: 2.80
-    // vermont: 3.85
-    // virginia: 3.70
-    // washington: 2.70
-    // westVirginia: 3.60
-    // wisconsin: 3.30
-    // wyoming: 2.90
+    "dc": 3.85,
+    "floridaNorth": 3.40,
+    "floridaSouth": 3.80,
+    "georgia": 3.25,
+    "idaho": 2.80,
+    "illinois": 3.25,
+    "indiana": 3.40,
+    "iowa": 2.80,
+    "kansas": 2.90,
+    "kentuckyEast": 4.05,
+    "kentuckyWest": 3.80,
+    "louisiana": 3.30,
+    "maine": 3.85,
+    "maryland": 3.85,
+    "massachusettes": 3.85,
+    "michiganNorth": 3.70,
+    "michiganSouth": 3.20,
+    "minnesota": 3.25,
+    "mississippi": 3,
+    "missouriEast": 3.50,
+    "missouriWest": 3.60,
+    "montana": 3.25,
+    "nebraska": 2.90,
+    "nevada": 2.75,
+    "newHampshire": 3.85,
+    "newJersey": 3.85,
+    "newMexico": 2.95,
+    "newYorkEast": 4,
+    "newYorkWest": 3.85,
+    "northDakota": 3.65,
+    "ohio": 3.25,
+    "oklahoma": 3.25,
+    "oregon": 2.75,
+    "pennsylvaniaEast": 3.70,
+    "pennsylvaniaWest": 3.10,
+    "rhodeIsland": 3.85,
+    "southCarolina": 3.50,
+    "southDakota": 3.20,
+    "tennessee": 3,
+    "texasEast": 3.10,
+    "texasWest": 3.50,
+    "utah": 2.80,
+    "vermont": 3.85,
+    "virginia": 3.70,
+    "washington": 2.70,
+    "westVirginia": 3.60,
+    "wisconsin": 3.30,
+    "wyoming": 2.90,
 
 };
 
 fuel = .68
 rate = acipcoRates[state]
 
-//var rate = 0;
-
+// this is the function i'm using to call the rate i need
 function rateArg(mileage) {
     return (mileage * (fuel + rate))
 }
-// cant figure out how to call the state when I run it
-// i'm not sure if this is the right format to list the sates and their values either. (probably not)
+
 
 console.log(rateArg(miles))
